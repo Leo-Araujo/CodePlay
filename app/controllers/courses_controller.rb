888af077db0e62a80741a5fa586540791cc26161
Leonardo <leonsx@hotmail.com>
2021-05-16 00:35:55 -0300

    Adiciona tela para cadastro de cursos

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,3 +6,20 @@ class CoursesController < ApplicationController
   def show
     @course = Course.find(params[:id])
   end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+    redirect_to @course
+    end
+  end
+
+  def course_params
+    params.require(:course).permit(:name, :description, :code, :price, :enrollment_deadline)
+  end
+end
